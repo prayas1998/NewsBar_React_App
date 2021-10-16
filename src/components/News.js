4e05643912ed26e170f0c6a2fa955aacee7d6094
prayas1998 <prayas98@gmail.com>
2021-10-16 19:36:03 +0530

    first commit

---
+++ src/components/News.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export class News extends Component {
+    //-------- Lecture #26 --------
+   
+    constructor() {
+        super();
+        console.log("Hello");
+
+        this.state = {
+            articles : [],
+            page: 1,
+            loading : false
+        }
+    }
+    
+    async componentDidMount() {
+
+        // This Executes after render()
+
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=c74f9444dc5f4826ac908c7f4f9dc3c6&page=1&pagesize=20";
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        console.log(parsedData);
+        this.setState({
+            articles: parsedData.articles,
+            totalResults: parsedData.totalResults
+        })
+            
+    }
+
+    handlePrevClick = async () => {
+        console.log("Prev");
+
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=c74f9444dc5f4826ac908c7f4f9dc3c6&page=${this.state.page - 1}&pagesize=20`;
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        console.log(parsedData);
+
+        this.setState({
+            page: this.state.page - 1,
+            articles: parsedData.articles
+        })
+    }
+
+    handleNextClick = async () => {
+        console.log("Next");
+
+        if(this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
+            
+        }
+        else{
+
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=c74f9444dc5f4826ac908c7f4f9dc3c6&page=${this.state.page + 1}&pagesize=20`;
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        console.log(parsedData);
+
+        this.setState({
+            page: this.state.page + 1,
+            articles: parsedData.articles
+        })
+        }
+    }
+
+    render() {
+        return (
+            <div className = "container my-3" >
+                <h2>NewsBar - Top Headlines</h2>
+
+                <div className="row">
+                    {this.state.articles.map((element)=>{
+                    return <div className = "col-md-4" key = {element.url}>
+                            <NewsItem title = {element.title?element.title.slice(0,45):" "} description = {element.description?element.description.slice(0,88):" "} imageUrl = {element.urlToImage} newsUrl = {element.url} />
+                        </div>
+                    })}
+                </div>
+                <div className="container d-flex justify-content-between my-3">
+                <button disabled = {this.state.page<=1} type="button" className="btn btn-dark"  onClick = {this.handlePrevClick}>&larr; Previous</button>
+                <button type="button" className="btn btn-dark" id="nextButton" onClick = {this.handleNextClick}>Next &rarr;</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default News
